483dceda4916283438d08051ec3ac7854f0904b6
Krishna Chaudhari <116093539+Krishnapc04@users.noreply.github.com>
2025-01-07 13:29:33 +0530

    Update const.jsx
    baseUrl changed

--- const.jsx
+++ const.jsx
@@ -1,4 +1,4 @@
-// export const baseURL = "https://composit-25-server.onrender.com";
-const baseURL = "http://localhost:5000";
+const baseURL = "https://composit-25-server.onrender.com";
+//const baseURL = "http://localhost:5000";
 
 export default baseURL;
